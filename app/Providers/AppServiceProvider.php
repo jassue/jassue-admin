34b5e4401e03d2af2db88e0e5ba488d5ef8d83da
jassue <jassue@163.com>
2021-06-10 09:24:09 +0000

    服务容器绑定

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,18 @@
 
 namespace App\Providers;
 
+use App\Domain\Admin\AdminService;
+use App\Domain\Admin\Repositories\AdminRepository;
+use App\Domain\Admin\Repositories\AdminRoleRepository;
+use App\Domain\Common\Services\MediaService;
+use App\Domain\Setting\Repositories\SettingRepository;
+use App\Domain\Setting\SettingService;
+use App\Domain\Staff\DepartmentService;
+use App\Domain\Staff\Repositories\DepartmentRepository;
+use App\Domain\Staff\Repositories\StaffRepository;
+use App\Domain\Staff\StaffService;
+use App\Domain\SystemLog\Repositories\SystemLogRepository;
+use App\Domain\SystemLog\SystemLogService;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -50,6 +62,44 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        // repositories
+        $this->app->singleton(AdminRepository::class, function () {
+            return new AdminRepository();
+        });
+        $this->app->singleton(AdminRoleRepository::class, function () {
+            return new AdminRoleRepository();
+        });
+        $this->app->singleton(SettingRepository::class, function () {
+            return new SettingRepository();
+        });
+        $this->app->singleton(StaffRepository::class, function () {
+            return new StaffRepository();
+        });
+        $this->app->singleton(DepartmentRepository::class, function () {
+            return new DepartmentRepository();
+        });
+        $this->app->singleton(SystemLogRepository::class, function () {
+            return new SystemLogRepository();
+        });
+
+        // services
+        $this->app->singleton(AdminService::class, function () {
+            return new AdminService();
+        });
+        $this->app->singleton(StaffService::class, function () {
+            return new StaffService();
+        });
+        $this->app->singleton(DepartmentService::class, function () {
+            return new DepartmentService();
+        });
+        $this->app->singleton(SettingService::class, function () {
+            return new SettingService();
+        });
+        $this->app->singleton(SystemLogService::class, function () {
+            return new SystemLogService();
+        });
+        $this->app->singleton(MediaService::class, function () {
+            return new MediaService();
+        });
     }
 }
